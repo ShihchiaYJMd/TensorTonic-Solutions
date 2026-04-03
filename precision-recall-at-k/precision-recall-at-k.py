92e79e5d20e20d170fefd674d5494777270cce13
Shining Yang <ShihchiaYoung@outlook.com>
2026-04-03 22:48:57 +0800

    Add solution for precision-recall-at-k

---
+++ precision-recall-at-k/precision-recall-at-k.py
@@ -0,0 +1,14 @@
+def precision_recall_at_k(recommended, relevant, k):
+    """
+    Compute precision@k and recall@k for a recommendation list.
+    """
+    # Write code here
+    top_k = recommended[: k]
+    relevant_set = set(relevant)
+
+    hits = sum(1 for item in top_k if item in relevant_set)
+    
+    prec = hits / k
+    recall = hits / len(relevant)
+    
+    return [prec, recall]
